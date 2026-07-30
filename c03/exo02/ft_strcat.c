9d944033a71552c61212819c098700dff76c3077
MJVHack <ritagabrieldaniel@gmail.com>
2026-07-30 21:51:18 +0200

    Optimizing

--- c03/exo02/ft_strcat.c
+++ c03/exo02/ft_strcat.c
@@ -6,7 +6,7 @@
 /*   By: evild <e.diabriek@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/24 18:24:05 by evild             #+#    #+#             */
-/*   Updated: 2026/07/24 21:02:01 by evild            ###   ########.fr       */
+/*   Updated: 2026/07/28 20:15:29 by evild            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ char	*ft_strcat(char *dest, char *src)
 
 	ptr = dest;
 	while (*dest)
-	{
 		dest++;
-	}
 	while (*src)
 	{
 		*dest = *src;
